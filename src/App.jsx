8c139afd5b116c56cfd60a5141f3263958db1a6a
Devendra Mali <devendra.mali@digihost.in>
2024-10-04 18:09:37 +0530

    footer section created

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from "react";
 import Navbar from "./layouts/Navbar";
 import Home from "./views/Home";
+import Footer from "./layouts/Footer";
 
 function App() {
   return (
     <>
       <Navbar />
       <Home />
+      <Footer/>
       {/* <h2>Best Rate Website</h2> */}
     </>
   );
---
+++ src/layouts/Footer.jsx
@@ -0,0 +1,244 @@
+import React from 'react'
+import { Container } from "react-bootstrap";
+// import  {Link}  from 'react-router-dom';
+
+const Footer = () => {
+  return (
+    <>
+        <div className='footer'>
+            <div className="footer-cta">
+                <Container>
+                    <div className="content">
+                        <h3 className="text">
+                        Looking for help?
+                        </h3>
+                        <h3 className="email">
+                            <a href="#!">sales@digihost.in</a>
+                        </h3>
+                    </div>
+                </Container>
+            </div>
+            <div className="footer-liks">
+                <Container>
+                    <div className="row">
+                        <div className="col-lg-2 col-md-3">
+                            <div className="footer-box">
+                                <h4>Product</h4>
+                                <ul>
+                                    <li><a href="#!">Designer</a></li>
+                                    <li><a href="#!">Page Building</a></li>
+                                    <li><a href="#!">CMS</a></li>
+                                    <li><a href="#!">Ecommerce</a></li>
+                                    <li><a href="#!">Interactions</a></li>
+                                    <li><a href="#!">SEO</a></li>
+                                    <li><a href="#!">Localization</a></li>
+                                    <li><a href="#!">Edit mode</a></li>
+                                    <li><a href="#!">Hosting</a></li>
+                                    <li><a href="#!">Dev link</a></li>
+                                    <li><a href="#!">Security</a></li>
+                                    <li><a href="#!">Accessibility</a></li>
+                                    <li><a href="#!">AI</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div className="col-lg-2 col-md-3">
+                            <div className="footer-box">
+                                <h4>Solutions</h4>
+                                <ul>
+                                    <li><a href="#!">Online Store</a></li>
+                                    <li><a href="#!">Online Booking</a></li>
+                                    <li><a href="#!">Restaurant Website</a></li>
+                                    <li><a href="#!">Blog Website</a></li>
+                                    <li><a href="#!">Portfolio Website</a></li>
+                                    <li><a href="#!">eCommerce</a></li>
+                                    <li><a href="#!">Enterprise Solutions</a></li>
+                                    <li><a href="#!">Student Website</a></li>
+                                    <li><a href="#!">Professional tools</a></li>
+                                    <li><a href="#!">Logo Maker</a></li>
+                                    <li><a href="#!">Agencies</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div className="col-lg-2 col-md-3">
+                            <div className="footer-box">
+                                <h4>Resources</h4>
+                                <ul>
+                                    <li><a href="#!">University</a></li>
+                                    <li><a href="#!">Blog</a></li>
+                                    <li><a href="#!">Customer Stories</a></li>
+                                    <li><a href="#!">Webinars and eBooks</a></li>
+                                    <li><a href="#!">Apps</a></li>
+                                    <li><a href="#!">Libraries</a></li>
+                                    <li><a href="#!">Templates</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div className="col-lg-2 col-md-3">
+                            <div className="footer-box">
+                                <h4>Company</h4>
+                                <ul>
+                                    <li><a href="#!">About us</a></li>
+                                    <li><a href="#!">Careers</a></li>
+                                    <li><a href="#!">Press</a></li>
+                                    <li><a href="#!">BestRate Shop</a></li>
+                                    <li><a href="#!">Terms of service</a></li>
+                                    <li><a href="#!">Privacy Policy</a></li>
+                                    <li><a href="#!">Cookie policy</a></li>
+                                    <li><a href="#!">Cookie preferences</a></li>
+                                    <li><a href="#!">Accessibility Statement</a></li>
+                                    <li><a href="#!">Sitemap</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div className="col-lg-2 col-md-3">
+                            <div className="footer-box">
+                                <h4>Community</h4>
+                                <ul>
+                                    <li><a href="#!">Discover the community</a></li>
+                                    <li><a href="#!">partner with BestRate</a></li>
+                                    <li><a href="#!">Certified partners</a></li>
+                                    <li><a href="#!">Become a Template Designer</a></li>
+                                    <li><a href="#!">become an affiliate</a></li>
+                                    <li><a href="#!">Become a Global Leader</a></li>
+                                    <li><a href="#!">Find a Meetup Near You</a></li>
+                                    <li><a href="#!">Cookie preferences</a></li>
+                                    <li><a href="#!">Accessibility Statement</a></li>
+                                    <li><a href="#!">Sitemap</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div className="col-lg-2 col-md-3">
+                            <div className="footer-box">
+                                <h4>Support</h4>
+                                <ul>
+                                    <li><a href="#!">Help center</a></li>
+                                    <li><a href="#!">Hire a professional</a></li>
+                                    <li><a href="#!">Report Abuse</a></li>
+                                    <li><a href="#!">System Status</a></li>
+                                    <li><a href="#!">become an affiliate</a></li>
+                                    <li><a href="#!">Become a Global Leader</a></li>
+                                    <li><a href="#!">Find a Meetup Near You</a></li>
+                                    <li><a href="#!">Cookie preferences</a></li>
+                                    <li><a href="#!">Accessibility Statement</a></li>
+                                    <li><a href="#!">Sitemap</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </Container>
+            </div>
+            <div className="footer-liks">
+                <Container>
+                    <div className="footer-box">
+                        <h4>Industries We serve</h4>
+                        <div className="row">
+                            <div className="col-lg-2 col-md-3">
+                                <ul>
+                                    <li><a href="#!">Carpentry</a></li>
+                                    <li><a href="#!">Civil</a></li>
+                                    <li><a href="#!">Electrical</a></li>
+                                    <li><a href="#!">Flooring</a></li>
+                                    <li><a href="#!">Furniture</a></li>
+                                    <li><a href="#!">Painting</a></li>
+                                    <li><a href="#!">Plumbing</a></li>
+                                    <li><a href="#!">Borewell</a></li>
+                                    <li><a href="#!">Building</a></li>
+                                    <li><a href="#!">Carpet</a></li>
+                                </ul>
+                            </div>
+                            <div className="col-lg-2 col-md-3">
+                                <ul>
+                                    <li><a href="#!">Fine Dining</a></li>
+                                    <li><a href="#!">Casual Dining</a></li>
+                                    <li><a href="#!">Fast Food</a></li>
+                                    <li><a href="#!">Fast Casual</a></li>
+                                    <li><a href="#!">Cafés & Coffee Shops</a></li>
+                                    <li><a href="#!">Ethnic Cuisine</a></li>
+                                    <li><a href="#!">Food Trucks</a></li>
+                                    <li><a href="#!">Buffets</a></li>
+                                    <li><a href="#!">Pop-Up Restaurants</a></li>
+                                    <li><a href="#!">Farm-to-Table</a></li>
+                                </ul>
+                            </div>
+                            <div className="col-lg-2 col-md-3">
+                                <ul>
+                                    <li><a href="#!">General Dentistry</a></li>
+                                    <li><a href="#!">Cosmetic Dentistry</a></li>
+                                    <li><a href="#!">Pediatric Dentistry</a></li>
+                                    <li><a href="#!">Orthodontics</a></li>
+                                    <li><a href="#!">Periodontics</a></li>
+                                    <li><a href="#!">Endodontics</a></li>
+                                    <li><a href="#!">Prosthodontics</a></li>
+                                    <li><a href="#!">Sedation Dentistry</a></li>
+                                    <li><a href="#!">Emergency Dentistry</a></li>
+                                    <li><a href="#!">Holistic Dentistry</a></li>
+                                </ul>
+                            </div>
+                            <div className="col-lg-2 col-md-3">
+                                <ul>
+                                    <li><a href="#!">Luxury</a></li>
+                                    <li><a href="#!">Boutique</a></li>
+                                    <li><a href="#!">Resorts</a></li>
+                                    <li><a href="#!">Budget</a></li>
+                                    <li><a href="#!">Business</a></li>
+                                    <li><a href="#!">All-Inclusive</a></li>
+                                    <li><a href="#!">Extended-Stay</a></li>
+                                    <li><a href="#!">Eco/Green</a></li>
+                                    <li><a href="#!">Hostels</a></li>
+                                    <li><a href="#!">Bed and Breakfasts</a></li>
+                                </ul>
+                            </div>
+                            <div className="col-lg-2 col-md-3">
+                                <ul>
+                                    <li><a href="#!">Residential Real Estate</a></li>
+                                    <li><a href="#!">Commercial Real Estate</a></li>
+                                    <li><a href="#!">Certified partners</a></li>
+                                    <li><a href="#!">Industrial Real Estate</a></li>
+                                    <li><a href="#!">Land and Lots</a></li>
+                                    <li><a href="#!">Luxury Real Estate</a></li>
+                                    <li><a href="#!">Mixed-Use Developments</a></li>
+                                    <li><a href="#!">Vacation/Short-Term Rentals</a></li>
+                                    <li><a href="#!">Multi-Family Real Estate</a></li>
+                                    <li><a href="#!">Retail Real Estate</a></li>
+                                </ul>
+                            </div>
+                            <div className="col-lg-2 col-md-3">
+                                <ul>
+                                    <li><a href="#!">Freight and Cargo Transport</a></li>
+                                    <li><a href="#!">Passenger Transport</a></li>
+                                    <li><a href="#!">Logistics and Supply Chain</a></li>
+                                    <li><a href="#!">Air Transportation</a></li>
+                                    <li><a href="#!">Maritime Transport</a></li>
+                                    <li><a href="#!">Rail Transport</a></li>
+                                    <li><a href="#!">Trucking and Road Transport</a></li>
+                                    <li><a href="#!">Courier and Parcel Services</a></li>
+                                    <li><a href="#!">Public Transport</a></li>
+                                    <li><a href="#!">Specialized Transport</a></li>  
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="social">
+                        <h2><a href="#!">Best Rate</a></h2>
+                        <ul>
+                            <li><a href="#!">Instagram</a></li>
+                            <li><a href="#!">LinkedIn</a></li>
+                            <li><a href="#!" className='twitter'><span>Twitter</span> X</a></li>
+                            <li><a href="#!">Facebook</a></li>
+                        </ul>
+                    </div>
+                    <div className="copyright">
+                        <p>© Copyright 2024 <a href="#!">DiigiiHost Tech Solutions Pvt. Ltd.</a> ALL RIGHTS RESERVED.</p>
+                        <div className="term">
+                            <a href="#!">Terms of Use</a>    
+                            <a href="#!">Privacy Policy</a>    
+                        </div>
+                    </div>
+                </Container>
+            </div>
+        </div>
+    </>
+  )
+}
+
+export default Footer;
