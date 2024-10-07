89bb8ad3c54b63691a1eb5cc31b4b76e9453562a
Devendra Mali <devendra.mali@digihost.in>
2024-10-07 11:14:46 +0530

    cta update

--- src/components/HomeComponents.jsx
+++ src/components/HomeComponents.jsx
@@ -107,20 +107,3 @@ export const ReadyToExplore = () => {
   );
 };
 
-export const Mission = () =>{
-  return(
-    <div className="section mission">
-      <div className="content">
-        <div className="container text-center">
-          <h2>Your Success is Our Mission</h2>
-          <a href="#!" className="button">Get Started</a>
-        </div>
-      </div>
-      <div className="back-text">
-        <h2>
-          INCIPERE
-          </h2>
-      </div>
-    </div>
-  )
-}
---
+++ src/layouts/Cta.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export const Cta = () => {
+    return(
+        <div className="section mission">
+          <div className="content">
+            <div className="container text-center">
+              <h2>Your Success is Our Mission</h2>
+              <a href="#!" className="button">Get Started</a>
+            </div>
+          </div>
+          <div className="back-text">
+            <h2>
+              INCIPERE
+              </h2>
+          </div>
+        </div>
+      )
+}
